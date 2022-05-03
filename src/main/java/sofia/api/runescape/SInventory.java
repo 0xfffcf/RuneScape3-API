f121bbdb6947735df3341384eecc8911c4112c1b
0xfffcf <101296800+0xfffcf@users.noreply.github.com>
2022-05-02 21:47:39 -0400

    Update SInventory.java

--- src/main/java/sofia/api/runescape/SInventory.java
+++ src/main/java/sofia/api/runescape/SInventory.java
@@ -7,7 +7,7 @@ import static kraken.plugin.api.Actions.MENU_EXECUTE_WIDGET;
 /**
  * Provides access to the local player's inventory.
  *
- * @author Sofia
+ * @author 0xfffcf
  * @version 1.0.0
  * @since 1.0.0
  */
